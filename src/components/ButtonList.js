64674fe09e3c495dc05a690ee774b1bdb6804ecb
Aman Sirohi <amansirohi077@gmail.com>
2024-01-09 18:09:44 +0530

    added scroll effect to button list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -7,35 +7,33 @@ const ButtonList = () => {
     const scrollRef = useRef(null);
 
     const prev = () => {
-      requestAnimationFrame(() => {
-        const scrollLeft = scrollRef.current.scrollLeft;
-        const itemWidth = parseInt(
-          getComputedStyle(scrollRef.current.children[0]).width
-        );
-        scrollRef.current.scrollLeft = scrollLeft - itemWidth * 3;
-      });
-    };
+        if (scrollRef.current) {
+          scrollRef.current.scrollBy({
+            left: -150,
+            behavior: "smooth",
+          });
+        }
+      };
 
-    const next = () => {
-      requestAnimationFrame(() => {
-        const scrollLeft = scrollRef.current.scrollLeft;
-        const itemWidth = parseInt(
-          getComputedStyle(scrollRef.current.children[0]).width
-        );
-        scrollRef.current.scrollLeft = scrollLeft + itemWidth * 3;
-      });
-    };
+      const next = () => {
+        if (scrollRef.current) {
+          scrollRef.current.scrollBy({
+            left: 150,
+            behavior: "smooth",
+          });
+        }
+      };
     const buttonList=["All", "Music", "Movie musicals", "Mixes", "Indian pop music", "Computer Programming", "Filmi", "Trains", "Street food", "Aircrafts"," Live", "Trucks", "History"]
     // , "News", "Tourist destinations"
     return (
-        <div className="flex  ml-[300px] z-40 bg-white pb-2 ">
+        <div className="flex  ml-[300px] z-40 bg-white pb-2 select-none">
           <button
             onClick={prev}
             className="hover:rounded-full w-10 h-10 mr-2 hover:bg-gray-100  "
           >
             <img alt="leftScrollBtn" className="inline-block" src={letScrollIcon} />
           </button>
-          <div ref={scrollRef} className="max-w-[86%] overflow-x-hidden flex  mx-5">
+          <div ref={scrollRef} className="max-w-[86%] overflow-x-scroll no-scrollbar flex  mx-5">
           {buttonList.map((buttonName, index)=><Button key={index} name={buttonName}/> )}
           </div>
           <button
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -16,8 +16,8 @@ const VideoCard = ({info}) => {
         <div>
         <li className="font-semibold py-2 text-[14px] line-clamp-2 max-h-[50px] leading-5">{title}</li>
         <li className='text-gray-500 text-[13px]'>{channelTitle}</li>
-        {!searchVideos?
-          ( <li className='text-gray-500 text-[13px]'>{info?.statistics?.viewCount ? formatCompactNumber(info?.statistics?.viewCount) : 0} views  {(Math.abs(new Date(publishedAt) - new Date()) / (60 * 60 * 24 * 1000)).toFixed(0)} days ago</li>):null}
+
+         {info.statistics && <li className='text-gray-500 text-[13px]'>{info?.statistics?.viewCount ? formatCompactNumber(info?.statistics?.viewCount) : 0} views  {(Math.abs(new Date(publishedAt) - new Date()) / (60 * 60 * 24 * 1000)).toFixed(0)} days ago</li>}
         </div>
 
         </ul>
